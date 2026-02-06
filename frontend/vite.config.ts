ca36384e24b234903fbd30d09b9fbd135bc56637
Samer Eladad <samerx96@gmail.com>
2026-02-06 16:29:16 +0100

    fix: hardcode empty VITE_API_BASE_URL in vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  define: {
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(''),
+  },
   server: {
     proxy: {
       '/api': {
